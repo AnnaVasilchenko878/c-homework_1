1d4a1395a163896983f187ae12bc507def9419e1
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-11-25 23:37:41 +0300

    Добавляет программу поиска максимального из трех чисел

---
+++ max_number/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double number3 = Convert.ToDouble(Console.ReadLine());
+double maxNumber = number1;
+
+if(maxNumber<number2) 
+{
+  maxNumber = number2;
+}
+if(maxNumber < number3)
+{
+  maxNumber = number3;
+} 
+Console.WriteLine($"Максимальное число: {maxNumber}");
